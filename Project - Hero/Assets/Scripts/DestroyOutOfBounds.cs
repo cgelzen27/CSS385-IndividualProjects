ca6ff14d1ad4e6b5feb00466fb7a770ab4d65c0a
Gelzen Crisanto <cgelzen@gmail.com>
2024-04-11 23:52:13 -0700

    Added projectile shooting

---
+++ Project - Hero/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float verticalBound = 10.0f;
+    private float horizontalBound = 16.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > verticalBound || transform.position.y < -verticalBound ||
+            transform.position.x > horizontalBound || transform.position.x < -horizontalBound)
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+}
